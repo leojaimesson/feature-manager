e9ef42d02fa7a9eb2e0530c838c37f0ef181df26
leojaimesson <leo.jaimesson.dev@gmail.com>
2023-01-21 20:38:45 -0300

    feat: create ProjectsPage

---
+++ frontend/src/main/factories/pages/ProjectsPageFactory.tsx
@@ -0,0 +1,5 @@
+import { ProjectsPage } from "../../../presenters/pages/ProjectsPage/ProjectsPage";
+
+export function makeProjectsPageFactory() {
+  return <ProjectsPage />;
+}
---
+++ frontend/src/presenters/pages/ProjectsPage/ProjectsPage.tsx
@@ -0,0 +1,9 @@
+type ProjectsPageProps = {};
+
+export function ProjectsPage({}: ProjectsPageProps) {
+  return (
+    <div>
+      <h1>Projects</h1>
+    </div>
+  );
+}
