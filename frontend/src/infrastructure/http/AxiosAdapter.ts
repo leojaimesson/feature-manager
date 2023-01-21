217e437f961960d39cba7b7ab4220c95539b23f6
leojaimesson <leo.jaimesson.dev@gmail.com>
2023-01-21 14:55:40 -0300

    feat: adds axios adapter

---
+++ frontend/src/infrastructure/http/AxiosAdapter.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from "./interfaces/HttpClient";
+import axios from "axios";
+
+export class AxiosAdpter implements HttpClient {
+  async get(url: string): Promise<any> {
+    const response = await axios.get(url);
+    return response.data;
+  }
+
+  async post(url: string, body: any): Promise<any> {
+    const response = await axios.post(url, body);
+    return response.data;
+  }
+
+  async put(url: string, body: any): Promise<any> {
+    const response = await axios.put(url, body);
+    return response.data;
+  }
+
+  async delete(url: string): Promise<any> {
+    const response = await axios.delete(url);
+    return response.data;
+  }
+}
